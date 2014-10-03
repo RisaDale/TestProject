90ba02bfca458b5063edc3e5c61c2c2e4e57a3bb
Risa Dale <rdale@dhcp-192-168-1-239.kavi>
2014-10-03 10:38:38 -0700

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Risa Dale on 10/3/14.
+//  Copyright (c) 2014 Risa Dale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Risa Dale on 10/3/14.
+//  Copyright (c) 2014 Risa Dale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
